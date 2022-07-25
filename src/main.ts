ee961770283340c666d3db27159807f52aaf3428
Clikuki <73450123+clikuki@users.noreply.github.com>
2022-07-25 12:02:21 +0800

    added different difficulties/modes

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,124 @@
 import { Symbols } from './symbols';
 import { Grid } from './grid.js';
 
-const mineCount = 20;
-let flagsLeft = mineCount;
-let hasClicked = false;
-let timeStart = 0;
-let gameIsStopped = false;
-
-const flagsLeftCounter = document.querySelector(
-	'.flagsLeft',
-) as HTMLParagraphElement;
-function updateFlagCounter() {
-	flagsLeftCounter.textContent = `${Symbols.flag} ${Math.max(flagsLeft, 0)}`;
-}
-updateFlagCounter();
-
-const timer = document.querySelector('.timer') as HTMLParagraphElement;
-requestAnimationFrame(function loop(t: number) {
-	requestAnimationFrame(loop);
-	if (!hasClicked || gameIsStopped) {
-		timeStart = t;
-		return;
+class FlagsLeft {
+	static elem = document.querySelector('.flagsLeft') as HTMLParagraphElement;
+	static counter = 0;
+	static updateDisplay() {
+		this.elem.textContent = `${Symbols.flag} ${Math.max(this.counter, 0)}`;
 	}
-	const seconds = Math.floor((t - timeStart) / 1000);
-	const milliseconds = `${Math.floor(
-		((t - timeStart) % 1000) / 10,
-	)}`.padStart(2, '0');
-	timer.textContent = `${seconds}:${milliseconds}`;
-});
+	static set(val: number) {
+		this.counter = val;
+		this.updateDisplay();
+	}
+	static crement(change: 1 | -1) {
+		this.counter += change;
+		this.updateDisplay();
+	}
+}
 
-const restartBtn = document.querySelector('.restart') as HTMLButtonElement;
+class Timer {
+	static elem = document.querySelector('.timer') as HTMLParagraphElement;
+	static isStarted = false;
+	static timeStart = 0;
+	static updateDisplay(t: number) {
+		const timeSince = t - this.timeStart;
+		const minutes = Math.floor(timeSince / 1000 / 60);
+		const seconds = `${Math.floor(timeSince / 1000) % 60}`.padStart(2, '0');
+		const milliseconds = `${Math.floor((timeSince % 1000) / 10)}`.padStart(
+			2,
+			'0',
+		);
+		this.elem.textContent = `${minutes}:${seconds}:${milliseconds}`;
+	}
+	static {
+		const That = this;
+		requestAnimationFrame(function loop(t: number) {
+			requestAnimationFrame(loop);
+			if (!That.isStarted) That.timeStart = t;
+			else That.updateDisplay(t);
+		});
+	}
+	static reset() {
+		this.isStarted = false;
+		this.timeStart = 0;
+		this.updateDisplay(0);
+	}
+	static start() {
+		this.isStarted = true;
+	}
+}
+
+interface difficultyInfo {
+	name: string;
+	mineCount: number;
+	width: number;
+	height: number;
+}
+class Difficulty {
+	static elem = document.querySelector('.difficulty') as HTMLButtonElement;
+	static difficulties: difficultyInfo[] = [
+		{
+			name: 'Easy',
+			mineCount: 10,
+			width: 8,
+			height: 8,
+		},
+		{
+			name: 'Normal',
+			mineCount: 20,
+			width: 10,
+			height: 10,
+		},
+		{
+			name: 'Hard',
+			mineCount: 30,
+			width: 12,
+			height: 12,
+		},
+	];
+	static curDifficulty = 1;
+	static current = this.difficulties[this.curDifficulty];
+	static updateDisplay() {
+		this.elem.textContent = `Difficulty: ${this.current.name}`;
+	}
+	static {
+		this.updateDisplay();
+		this.elem.addEventListener('click', () => {
+			this.curDifficulty =
+				++this.curDifficulty % this.difficulties.length;
+			this.current = this.difficulties[this.curDifficulty];
+			this.updateDisplay();
+			restart();
+		});
+	}
+}
+
+const container = document.querySelector('.container') as HTMLElement;
 function newGrid() {
-	gameIsStopped = false;
 	let grid = new Grid(
-		10,
-		10,
-		mineCount,
-		document.querySelector('.container')!,
+		Difficulty.current.width,
+		Difficulty.current.height,
+		Difficulty.current.mineCount,
+		container,
 		({ isFlagged, isUnflagged }) => {
-			hasClicked = true;
-			if (isFlagged) {
-				flagsLeft--;
-				updateFlagCounter();
-			} else if (isUnflagged) {
-				flagsLeft++;
-				updateFlagCounter();
-			}
+			Timer.start();
+			if (isUnflagged) FlagsLeft.crement(1);
+			else if (isFlagged) FlagsLeft.crement(-1);
 			if (grid.hasWon || grid.hasLost) {
-				gameIsStopped = true;
 				return;
 			}
 		},
 	);
 }
-restartBtn.addEventListener('click', () => {
+
+function restart() {
+	Timer.reset();
+	FlagsLeft.set(Difficulty.current.mineCount);
 	newGrid();
-	hasClicked = false;
-	timer.textContent = '0:00';
-	flagsLeft = mineCount;
-	updateFlagCounter();
-});
+}
+
+const restartBtn = document.querySelector('.restart') as HTMLButtonElement;
+restartBtn.addEventListener('click', restart);
+
 newGrid();
